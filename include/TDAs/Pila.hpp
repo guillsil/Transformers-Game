f7390d416b2896186dece5df6994ea2446f88554
thvyyr <111441275+guillsil@users.noreply.github.com>
2024-11-13 16:57:55 -0300

    Pila.hpp paso las pruebas , está bien

--- include/TDAs/Pila.hpp
+++ include/TDAs/Pila.hpp
@@ -5,6 +5,8 @@
 #include <stdexcept>
 #include "NodoLista.hpp"
 
+const std::string ERROR_PILA_VACIA = "ERROR: La pila está vacia";
+
 class ExcepcionPila : public std::runtime_error {
 public:
     ExcepcionPila(std::string mensaje): runtime_error(mensaje) {
@@ -56,30 +58,63 @@ public:
 
 template<typename T>
 Pila<T>::Pila() {
+    ultimo_nodo = nullptr;
+    cantidad_datos = 0;
 }
 
 template<typename T>
 void Pila<T>::alta(T dato) {
+    NodoLista<T>* nuevo_nodo = new NodoLista<T>(dato);
+    if(vacio()){
+        ultimo_nodo = nuevo_nodo;
+        ultimo_nodo->cambiar_anterior(nullptr);
+    }
+    else {
+        nuevo_nodo->cambiar_anterior(ultimo_nodo);
+        ultimo_nodo = nuevo_nodo;
+    }
+    cantidad_datos++;
 }
 
 template<typename T>
 T Pila<T>::baja() {
+    if(vacio()){
+        throw ExcepcionPila();
+    }
+    NodoLista<T>* nodo_anterior = ultimo_nodo->obtener_anterior();
+    T dato_eliminado = ultimo_nodo->obtener_dato();
+    delete ultimo_nodo;
+    ultimo_nodo = nodo_anterior;
+    cantidad_datos--;
+    return dato_eliminado;
 }
 
 template<typename T>
 T Pila<T>::primero() {
+    if(vacio()){
+        throw ExcepcionPila(ERROR_PILA_VACIA);
+    }
+    return ultimo_nodo->obtener_dato();
 }
 
 template<typename T>
 size_t Pila<T>::tamanio() {
+    return cantidad_datos;
 }
 
 template<typename T>
 bool Pila<T>::vacio() {
+    return (cantidad_datos == 0);
 }
 
 template<typename T>
 Pila<T>::~Pila() {
+    NodoLista<T>* nodo_liberar;
+    for (int i = 0; i < cantidad_datos; i++){
+        nodo_liberar = ultimo_nodo->obtener_anterior();
+        delete ultimo_nodo;
+        ultimo_nodo = nodo_liberar;
+    }
 }
 
 #endif
