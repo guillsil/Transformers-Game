ea53b3e1e80b03104b41bac378284a80432cded6
thvyyr <111441275+guillsil@users.noreply.github.com>
2024-11-13 16:57:18 -0300

    Cola.hpp paso las pruebas y está bien.

--- include/TDAs/Cola.hpp
+++ include/TDAs/Cola.hpp
@@ -4,6 +4,8 @@
 #include <cstddef>
 #include <stdexcept>
 #include "NodoLista.hpp"
+#include "Pila.hpp"
+const std::string ERROR_COLA_VACIA = "ERROR: La cola está vacia";
 
 class ExcepcionCola : public std::runtime_error {
 public:
@@ -17,6 +19,7 @@ public:
 template<typename T>
 class Cola {
 private:
+
     NodoLista<T>* primer_nodo;
     NodoLista<T>* ultimo_nodo;
     size_t cantidad_datos;
@@ -57,30 +60,66 @@ public:
 
 template<typename T>
 Cola<T>::Cola() {
+    primer_nodo = nullptr;
+    ultimo_nodo = nullptr;
+    cantidad_datos = 0;
 }
 
 template<typename T>
 void Cola<T>::alta(T dato) {
+    NodoLista<T>* nodo_nuevo = new NodoLista<T>(dato);
+    nodo_nuevo->cambiar_siguiente(nullptr);
+    if(cantidad_datos == 0){
+        primer_nodo = nodo_nuevo;
+        ultimo_nodo = primer_nodo;
+    }
+    else{
+        NodoLista<T>* nodo_anterior_ultimo = ultimo_nodo;
+        ultimo_nodo = nodo_nuevo;
+        nodo_anterior_ultimo->cambiar_siguiente(ultimo_nodo);
+    }
+    cantidad_datos++;
 }
 
 template<typename T>
 T Cola<T>::baja() {
+    if(vacio()){
+        throw ExcepcionCola(ERROR_COLA_VACIA);
+    }
+    NodoLista<T>* nodo_eliminar = primer_nodo;
+    T dato_eliminado  = nodo_eliminar->obtener_dato();
+    primer_nodo = primer_nodo->obtener_siguiente();
+    delete nodo_eliminar;
+    cantidad_datos--;
+    return dato_eliminado;
 }
 
 template<typename T>
 T Cola<T>::primero() {
+    if(vacio()){
+        throw ExcepcionCola(ERROR_COLA_VACIA);
+    }
+    return primer_nodo->obtener_dato();
 }
 
 template<typename T>
 size_t Cola<T>::tamanio() {
+    return cantidad_datos;
 }
 
 template<typename T>
 bool Cola<T>::vacio() {
+    return (cantidad_datos == 0);
 }
 
 template<typename T>
 Cola<T>::~Cola() {
+    NodoLista<T>* nodo_liberar;
+    for (int i = 0; i < cantidad_datos; i++){
+        nodo_liberar = primer_nodo->obtener_siguiente();
+        delete primer_nodo;
+        primer_nodo = nodo_liberar;
+    }
 }
 
 #endif
