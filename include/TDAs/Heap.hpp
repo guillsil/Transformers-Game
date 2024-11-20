92a3b774253b5190b2f2028f9855e3a1ac26efb7
guillsil <guasilva@fi.uba.ar>
2024-11-20 11:17:40 -0300

    Fix heap

--- include/TDAs/Heap.hpp
+++ include/TDAs/Heap.hpp
@@ -5,6 +5,10 @@
 #include <stdexcept>
 #include "Vector.hpp"
 
+const bool MAXIMA = true;
+const bool MINIMA = false;
+const std::string ERROR_ESTA_VACIO = "ERROR:: Esta vácio";
+
 class ExcepcionHeap : public std::runtime_error {
 public:
     ExcepcionHeap(std::string mensaje): runtime_error(mensaje) {
@@ -19,6 +23,18 @@ template<typename T>
 class Heap {
 private:
     Vector<T> datos;
+    bool operador;
+    //Pre:
+    //Post:
+    T baja_recursiva(bool maxima, size_t indice, size_t hijo_izquierdo, size_t hijo_derecho);
+
+    //Pre: -
+    //Post: Se intercambia el elemento en la posición indice1 con el de la posición indice2.
+    void intercambiar(size_t indice1, size_t indice2);
+
+    //Pre: Índice debe estar en el rango 0 y tamaño de los datos.
+    //Post: El elemento en la posición índice (Sube) hacia su posición correcta en el heap
+    void subir(size_t indice);
 
 public:
     // Constructor default.
@@ -52,32 +68,110 @@ public:
     ~Heap();
 };
 
+template <typename T>
+void Heap<T>::intercambiar(size_t indice1, size_t indice2){
+    T aux = datos[indice1];
+    datos[indice1] = datos[indice2];
+    datos[indice2] = aux;
+}
+
+template <typename T>
+void Heap<T>::subir(size_t indice){
+    size_t padre = (indice - 1) / 2;
+    if(operador){
+        while (datos[indice] > datos[padre] && indice != 0){
+            intercambiar(indice, padre);
+            indice = padre;
+            if(indice != 0) padre = (indice - 1) / 2;
+        }
+    }else{
+        while (datos[indice] < datos[padre] && indice != 0){
+            intercambiar(indice, padre);
+            indice = padre;
+            if(indice != 0) padre = (indice - 1) / 2;
+        }
+    }
+}
+
+template<typename T>
+T Heap<T>::baja_recursiva(bool maxima, size_t indice, size_t hijo_izquierdo, size_t hijo_derecho){
+    if(tamanio() == 1) return datos.baja();
+    if(indice == 0){
+        intercambiar(indice, tamanio() - 1);
+    }
+    if(maxima){
+        if(hijo_izquierdo < tamanio() && hijo_izquierdo != tamanio() - 1){
+            if(hijo_derecho < tamanio() && hijo_derecho != tamanio() - 1){
+                if(datos[hijo_derecho] > datos[hijo_izquierdo]){
+                    intercambiar(indice, hijo_derecho);
+                    return baja_recursiva(maxima, hijo_derecho, hijo_derecho * 2 + 1, hijo_derecho * 2 + 2);
+                }
+                intercambiar(indice, hijo_izquierdo);
+                return baja_recursiva(maxima, hijo_izquierdo, hijo_izquierdo * 2 + 1, hijo_izquierdo * 2 + 2);
+            }
+            intercambiar(indice, hijo_izquierdo);
+            return baja_recursiva(maxima, hijo_izquierdo, hijo_izquierdo * 2 + 1, hijo_izquierdo * 2 + 2);
+        }
+        return datos.baja();
+    }
+    if(hijo_izquierdo < tamanio() && hijo_izquierdo != tamanio() - 1){
+        if(hijo_derecho < tamanio() && hijo_derecho != tamanio() - 1){
+            if(datos[hijo_derecho] < datos[hijo_izquierdo]){
+                intercambiar(indice, hijo_derecho);
+
+                return baja_recursiva(maxima, hijo_derecho, hijo_derecho * 2 + 1, hijo_derecho * 2 + 2);
+            }
+            intercambiar(indice, hijo_izquierdo);
+            return baja_recursiva(maxima, hijo_izquierdo, hijo_izquierdo * 2 + 1, hijo_izquierdo * 2 + 2);
+        }
+        intercambiar(indice, hijo_izquierdo);
+        return baja_recursiva(maxima, hijo_izquierdo, hijo_izquierdo * 2 + 1, hijo_izquierdo * 2 + 2);
+    }
+    return datos.baja();
+}
+
+
+
 template<typename T>
 Heap<T>::Heap() {
+    operador = MAXIMA;
 }
 
 template<typename T>
 Heap<T>::Heap(bool maxima) {
+    operador = maxima;
 }
 
 template<typename T>
 void Heap<T>::alta(T dato) {
+    datos.alta(dato);
+    if(tamanio() != 1){
+        subir(tamanio() - 1);
+    }
 }
 
 template<typename T>
 T Heap<T>::baja() {
+    if(vacio())
+        throw ExcepcionHeap(ERROR_ESTA_VACIO);
+    return baja_recursiva(operador, 0, 1, 2);
 }
 
 template<typename T>
 T Heap<T>::primero() {
+    if(vacio())
+        throw ExcepcionHeap(ERROR_ESTA_VACIO);
+    return datos[0];
 }
 
 template<typename T>
 bool Heap<T>::vacio() {
+    return datos.vacio();
 }
 
 template<typename T>
 size_t Heap<T>::tamanio() {
+    return datos.tamanio();
 }
 
 template<typename T>
