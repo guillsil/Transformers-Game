24bff02ebe57af46bc1a42b6595716ef0a2a9060
CandelaRiveroA <criveroa@fi.uba.ar>
2024-12-02 17:08:54 -0300

    Cambiando nombre bool transformado -> esta_transformado
    Agregado comentarios y espaciado

--- src/Transformers.cpp
+++ src/Transformers.cpp
@@ -1,37 +1,37 @@
 #include "Transformers.hpp"
 
 Transformers::Transformers(std::string nombre, int fuerza, int defensa, int velocidad,
-                           std::string faccion, std::string vehiculo, bool transformado): estadisticas(fuerza, defensa, velocidad){
-    this->nombre = nombre;
-    this->faccion = faccion;
-    this->vehiculo = vehiculo;
-    this->transformado = transformado;
+                           std::string faccion, std::string vehiculo, bool esta_transformado): estadisticas(fuerza, defensa, velocidad){
+    this-> nombre = nombre;
+    this-> faccion = faccion;
+    this-> vehiculo = vehiculo;
+    this-> esta_transformado = esta_transformado;
 }
 
 Transformers::Transformers(std::string nombre, int fuerza, int defensa, int velocidad,
                            std::string faccion, std::string vehiculo): estadisticas(fuerza, defensa, velocidad){
-    this->nombre = nombre;
-    this->faccion = faccion;
-    this->vehiculo = vehiculo;
-    this->transformado = false;
+    this-> nombre = nombre;
+    this-> faccion = faccion;
+    this-> vehiculo = vehiculo;
+    this-> esta_transformado = false;
 }
 
 Transformers::Transformers(Vector<std::string>& elementos): estadisticas( std::stoi(elementos[1]), std::stoi(elementos[2]), std::stoi(elementos[3])){
-    this->nombre = elementos[0];
-    this->faccion = elementos[4];
-    this->vehiculo = elementos[5];
-    this->transformado = pase_texto_bool_transformado(elementos[6]);
+    this-> nombre = elementos[0];
+    this-> faccion = elementos[4];
+    this-> vehiculo = elementos[5];
+    this-> esta_transformado = pase_texto_a_bool_transformado(elementos[6]);
 }
 
 Transformers::Transformers(): estadisticas(5,5,5){
-    this->nombre = "Sparkling";
-    this->faccion = FACCION_AUTOBOTS;
-    this->vehiculo = VEHICULO_AUTO;
-    this->transformado = false;  
+    this-> nombre = "Sparkling";
+    this-> faccion = FACCION_AUTOBOTS;
+    this-> vehiculo = VEHICULO_AUTO;
+    this-> esta_transformado = false;  
 }
 
 std::string Transformers::obtener_nombre(){
-    return this->nombre;
+    return this-> nombre;
 }
 
 size_t Transformers::obtener_poder(){
@@ -39,60 +39,59 @@ size_t Transformers::obtener_poder(){
 }
 
 void Transformers::cambiar_forma(){
-    if (transformado){ //Si el estado anterior a transformado es true, pasa a ser false; 
-        this->transformado = false;
+    if (esta_transformado){ //Si el estado anterior a transformado es true, pasa a ser false; 
+        this-> esta_transformado = false;
     } else { //Si el estado anterior a transformado es false, pasa a true.
-        this->transformado = true;
+        this-> esta_transformado = true;
     }
-    chequeo_vehiculo();
-    
+    chequeo_vehiculo();   
 }
 
 Transformers& Transformers::operator=(const Transformers &otro_transformer){
     if (this != &otro_transformer){
-        this->nombre = otro_transformer.nombre;
-        this->estadisticas = otro_transformer.estadisticas;
-        this->faccion = otro_transformer.faccion;
-        this->vehiculo = otro_transformer.vehiculo;
-        this->transformado = otro_transformer.transformado;
+        this-> nombre = otro_transformer.nombre;
+        this-> estadisticas = otro_transformer.estadisticas;
+        this-> faccion = otro_transformer.faccion;
+        this-> vehiculo = otro_transformer.vehiculo;
+        this-> esta_transformado = otro_transformer.esta_transformado;
     }
     return *this;
 }
 
 bool Transformers::operator==(const Transformers &otro_transformer){
-    bool facciones_iguales = false;
-    if (this->faccion == otro_transformer.faccion){
-        facciones_iguales = true;
+    bool son_facciones_iguales = false;
+    if (this-> faccion == otro_transformer.faccion){
+        son_facciones_iguales = true;
     }
-    return facciones_iguales;
+    return son_facciones_iguales;
 }
 
 bool Transformers::son_iguales_fuerza(Transformers otro_transformer){
-    bool transformers_iguales = false;
+    bool son_transformers_iguales = false;
     int contador_estaditicas_iguales = 0;
 
-    if (this->estadisticas.obtener_fuerza() == otro_transformer.estadisticas.obtener_fuerza()) {contador_estaditicas_iguales++;  /*std::cout << "Fuerza igual "; */}
-    if (this->estadisticas.obtener_defensa() == otro_transformer.estadisticas.obtener_defensa()) {contador_estaditicas_iguales++; /*std::cout << "Defensa igual ";*/}
-    if (this->estadisticas.obtener_velocidad() == otro_transformer.estadisticas.obtener_velocidad()) {contador_estaditicas_iguales++; /*std::cout << "Velocidad igual ";*/}
+    if (this-> estadisticas.obtener_fuerza() == otro_transformer.estadisticas.obtener_fuerza()) {contador_estaditicas_iguales++;  /*std::cout << "Fuerza igual "; */}
+    if (this-> estadisticas.obtener_defensa() == otro_transformer.estadisticas.obtener_defensa()) {contador_estaditicas_iguales++; /*std::cout << "Defensa igual ";*/}
+    if (this-> estadisticas.obtener_velocidad() == otro_transformer.estadisticas.obtener_velocidad()) {contador_estaditicas_iguales++; /*std::cout << "Velocidad igual ";*/}
     
     if (contador_estaditicas_iguales >= 2){
-        transformers_iguales = true;
+        son_transformers_iguales = true;
     }
-    return transformers_iguales;
+    return son_transformers_iguales;
 }
 
 bool Transformers::es_mas_fuerte(Transformers otro_transformer){
-    bool transformer_actual_es_mas_fuerte = false;
+    bool es_transformer_actual_es_mas_fuerte = false;
     int contador_estadidicas_mas_altas = 0;
 
-    if (this->estadisticas.obtener_fuerza() > otro_transformer.estadisticas.obtener_fuerza()) {contador_estadidicas_mas_altas++; /*std::cout << "Fuerza superior ";*/}
-    if (this->estadisticas.obtener_defensa() > otro_transformer.estadisticas.obtener_defensa()) {contador_estadidicas_mas_altas++; /*std::cout << "Defensa superior ";*/}
-    if (this->estadisticas.obtener_velocidad() > otro_transformer.estadisticas.obtener_velocidad()) {contador_estadidicas_mas_altas++; /*std::cout << "Velocidad superior "*/ }
+    if (this-> estadisticas.obtener_fuerza() > otro_transformer.estadisticas.obtener_fuerza()) {contador_estadidicas_mas_altas++; /*std::cout << "Fuerza superior ";*/}
+    if (this-> estadisticas.obtener_defensa() > otro_transformer.estadisticas.obtener_defensa()) {contador_estadidicas_mas_altas++; /*std::cout << "Defensa superior ";*/}
+    if (this-> estadisticas.obtener_velocidad() > otro_transformer.estadisticas.obtener_velocidad()) {contador_estadidicas_mas_altas++; /*std::cout << "Velocidad superior "*/ }
     
     if (contador_estadidicas_mas_altas >= 2){
-        transformer_actual_es_mas_fuerte = true;
+        es_transformer_actual_es_mas_fuerte = true;
     }
-    return transformer_actual_es_mas_fuerte;
+    return es_transformer_actual_es_mas_fuerte;
 }
 
 int Transformers::calcular_bonificacion(int estadistica_base, int multiplicador, int indicador_aumento_reduccion){
@@ -102,13 +101,13 @@ int Transformers::calcular_bonificacion(int estadistica_base, int multiplicador,
 void Transformers::chequeo_vehiculo(){
     //El caso normal = incremento = 1;
     int interruptor_incremento_reduccion = 1;
-    if (!transformado){
+    if (!esta_transformado){
         //El caso al reves = reduccion = -1
         interruptor_incremento_reduccion = -1;
     } 
     //Estructura de la suma: Estaditica_a_cambiar = estadistica_a_cambiar + (1 ó -1)*(aumento/reduccion base * multiplicador)
-    if (this->faccion == FACCION_AUTOBOTS){
-        if(this->vehiculo == VEHICULO_AUTO){ // F- D- V++
+    if (this-> faccion == FACCION_AUTOBOTS){
+        if(this-> vehiculo == VEHICULO_AUTO){ // F- D- V++
             this-> estadisticas.aplicar_bonificacion_distintas(
                 calcular_bonificacion(ESTADISTICAS_REDUCCION,CAMBIO_NORMAL,interruptor_incremento_reduccion),
                 calcular_bonificacion(ESTADISTICAS_REDUCCION,CAMBIO_NORMAL,interruptor_incremento_reduccion),
@@ -120,7 +119,7 @@ void Transformers::chequeo_vehiculo(){
                 calcular_bonificacion(ESTADISTICAS_REDUCCION,CAMBIO_NORMAL,interruptor_incremento_reduccion));
         }
     } else { //Caso Decepticons
-        if (this->vehiculo == VEHICULO_AVION){ // F++ D--- V++
+        if (this-> vehiculo == VEHICULO_AVION){ // F++ D--- V++
         this-> estadisticas.aplicar_bonificacion_distintas(
                 calcular_bonificacion(ESTADISTICAS_AUMENTO,CAMBIO_MUCHO,interruptor_incremento_reduccion),
                 calcular_bonificacion(ESTADISTICAS_REDUCCION,CAMBIO_TOTALMENTE,interruptor_incremento_reduccion),
@@ -134,7 +133,7 @@ void Transformers::chequeo_vehiculo(){
     }
 }
 
-bool Transformers::pase_texto_bool_transformado(std::string texto_transformado){
+bool Transformers::pase_texto_a_bool_transformado(std::string texto_transformado){
     bool estado_actual;
     if (texto_transformado == "Si"){
         estado_actual = true;
@@ -149,7 +148,7 @@ void Transformers::mostrar(){
     std::cout << "  Faccion: " << faccion;
     std::cout << "  Vehiculo: " << vehiculo; 
     std::cout << "  Transformado: ";
-    if (transformado){
+    if (esta_transformado){
         std::cout << "Si";
     } else {
         std::cout << "No";
@@ -165,8 +164,8 @@ std::ostream &operator<<(std::ostream &os, const Transformers &transformer){
         << "," << transformer.faccion
         << "," << transformer.vehiculo
         << ",";
-    
-    if (transformer.transformado){
+    //Codigo duplicado. Problemas para llamar a metodos cuando se pasa como const
+    if (transformer.esta_transformado){
         os << "Si";
     } else {
         os << "No";
